8c3e80a1ee5af12e9ab12eee47b39dd69103806a
mdshakilhossain720 <mdshakilhossain720@gmail.com>
2026-01-08 16:49:24 +0600

    counter app add

---
+++ lib/riverpod/counterapp.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/// Root Widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Counter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Counter App'),
+    );
+  }
+}
+
+/// Home Screen (Stateful)
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+/// State class
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  /// Counter increment function
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  /// Counter decrement function (extra)
+  void _decrementCounter() {
+    setState(() {
+      if (_counter > 0) {
+        _counter--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            const SizedBox(height: 10),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  child: const Text('-'),
+                ),
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text('+'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
